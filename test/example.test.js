4af308185b21e9859da4d607f73b069d1e14002b
kathrynzaro <zarokathryn@gmail.com>
2022-05-20 15:58:58 -0700

    adding renderItem should add class if bought test that i forgot

--- test/example.test.js
+++ test/example.test.js
@@ -18,3 +18,17 @@ test('renderItem should return a div', (expect) => {
     // Make assertions about what is expected versus the actual result
     expect.equal(actual.outerHTML, expected);
 });
+
+test('renderItem should add class if bought', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = `<div class="complete">77 peppers</div>`;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderItem({ qty: 77, name: 'peppers', purchased: true });
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
